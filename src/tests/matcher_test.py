7a38c64f216609cd5e4a0eb7ad9c06bea3d49b05
Jannis Mosshammer <jannis.mosshammer@netways.de>
2012-12-12 15:15:40 +0100

    pylint refactoring

--- src/tests/matcher_test.py
+++ src/tests/matcher_test.py
@@ -34,42 +34,42 @@ class MatcherTestCase(unittest.TestCase):
     
         current_matcher = Matcher("(message is 'i am a testmessage')")
 
-        testEvent = Event("I AM A Testmessage")
-        assert current_matcher.matches(testEvent) == True
+        test_event = Event("I AM A Testmessage")
+        assert current_matcher.matches(test_event) == True
 
-        testEvent = Event("I AM A non matching Testmessage")
-        assert current_matcher.matches(testEvent) == False
+        test_event = Event("I AM A non matching Testmessage")
+        assert current_matcher.matches(test_event) == False
         
         current_matcher = Matcher("(message is 'i am a testmessage') OR (source in ('snmp')) AND test = 123")
-        testEvent = Event("I AM A Testmessage")
+        test_event = Event("I AM A Testmessage")
         # Should be true as the left part is not evaluated
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
-        testEvent = Event("I AM A non matching Testmessage")
-        assert current_matcher.matches(testEvent) == False
+        test_event = Event("I AM A non matching Testmessage")
+        assert current_matcher.matches(test_event) == False
 
         current_matcher = Matcher("(message starts with 'i am a testmessage') OR message ends with 'test123'")
         
-        testEvent = Event("I AM A non matching Testmessage")
-        assert current_matcher.matches(testEvent) == False
+        test_event = Event("I AM A non matching Testmessage")
+        assert current_matcher.matches(test_event) == False
 
-        testEvent = Event("I AM A Testmessage")
-        assert current_matcher.matches(testEvent) == True
-        testEvent = Event("Idsfdsfdsfdsfdsf AM A Testmessage test123")
+        test_event = Event("I AM A Testmessage")
+        assert current_matcher.matches(test_event) == True
+        test_event = Event("Idsfdsfdsfdsfdsf AM A Testmessage test123")
 
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("(message REGEXP 'Test\w+ [test]+123$' )")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
 
     def test_matcher_memory(self):
         """ Tests if regular expression groups are stored in memory after a match
             
         """
-        testEvent = Event("test123 AM A Testmessage test123")
+        test_event = Event("test123 AM A Testmessage test123")
 
         current_matcher = Matcher("(message REGEXP '(?P<Group1>.*) AM A Testmessage' )")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         assert current_matcher["Group1"] == "test123"
    
     def test_simple_message_negate(self):
@@ -77,111 +77,111 @@ class MatcherTestCase(unittest.TestCase):
 
         """
         current_matcher = Matcher("(message is not 'i am a testmessage')")
-        testEvent = Event("I AM A Testmessage")
-        assert current_matcher.matches(testEvent) == False
+        test_event = Event("I AM A Testmessage")
+        assert current_matcher.matches(test_event) == False
 
-        testEvent = Event("I AM A non matching Testmessage")
-        assert current_matcher.matches(testEvent) == True
+        test_event = Event("I AM A non matching Testmessage")
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("(message is not 'i am a testmessage') OR (source in ('snmp')) AND test = 123")        
-        testEvent = Event("I AM A Testmessage")
+        test_event = Event("I AM A Testmessage")
         # Should be true as the left part is not evaluated
-        assert current_matcher.matches(testEvent) == False
+        assert current_matcher.matches(test_event) == False
         
-        testEvent = Event("I AM A non matching Testmessage")
-        assert current_matcher.matches(testEvent) == True
+        test_event = Event("I AM A non matching Testmessage")
+        assert current_matcher.matches(test_event) == True
 
     def test_simple_numeric(self):
         """ Simple tests for numeric values (float and int)
 
         """
-        testEvent = Event("ignore my message", time.ctime(),{
+        test_event = Event("ignore my message", time.ctime(),{
             "severity" : 4,
             "availability" : 0.995
         })
         
         current_matcher = Matcher("severity > 3")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("severity = 3")
-        assert current_matcher.matches(testEvent) == False
+        assert current_matcher.matches(test_event) == False
         
         current_matcher = Matcher("severity >= 4")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("severity <= 4")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("severity = 4")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("severity != 4")
-        assert current_matcher.matches(testEvent) == False
+        assert current_matcher.matches(test_event) == False
         
         current_matcher = Matcher("severity != 3")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("availability > 0.99")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("availability < 1")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
     def test_simple_set(self):
         """ Simple tests for set operations 
 
         """
-        testEvent = Event("ignore my message", time.ctime(),{
+        test_event = Event("ignore my message", time.ctime(),{
             "severity" : 6,
             "source" : 'snmp'
         })
         current_matcher = None
 
         current_matcher = Matcher("severity IN (1, 2, 3, 4, 5)")
-        assert current_matcher.matches(testEvent) == False
+        assert current_matcher.matches(test_event) == False
 
         current_matcher = Matcher("severity IN (1, 2, 3, 4, 5, 6)")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
 
         current_matcher = Matcher("severity NOT IN (1, 2, 3, 4, 5)")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
 
         current_matcher = Matcher("severity NOT IN (1, 2, 3, 4, 5, 6)")
-        assert current_matcher.matches(testEvent) == False
+        assert current_matcher.matches(test_event) == False
 
         
         current_matcher = Matcher("source IN ('j2ee','snmp')")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
 
         current_matcher = Matcher("source NOT IN ('snmp','j2ee')")
-        assert current_matcher.matches(testEvent) == False
+        assert current_matcher.matches(test_event) == False
 
     def test_ip_operators(self):
         """ IP operator tests
             
         """
-        testEvent = Event("blobb", time.ctime(),{
+        test_event = Event("blobb", time.ctime(),{
             "address": '192.168.67.3',
             
         })
         
         current_matcher = Matcher("address IN NETWORK '192.168.0.0/16'")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("address NOT IN NETWORK '192.168.1.0/24'")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
         
         current_matcher = Matcher("address IN NETWORK '192.168.1.0/24'")
-        assert current_matcher.matches(testEvent) == False
+        assert current_matcher.matches(test_event) == False
         
         current_matcher = Matcher("address IN IP RANGE '192.168.1.0-192.169.1.1'")
-        assert current_matcher.matches(testEvent) == True
+        assert current_matcher.matches(test_event) == True
 
     def test_mixed(self):
         """ Tests of several conjunctions and different operators
 
         """
-        testEvent = Event("[LINK DOWN] eth0 on localhost is down.", time.ctime(),{
+        test_event = Event("[LINK DOWN] eth0 on localhost is down.", time.ctime(),{
             "severity" : 7,
             "facility" : 4,
             "host" : "localhost",
@@ -189,13 +189,13 @@ class MatcherTestCase(unittest.TestCase):
         })
         
         current_matcher = Matcher("message REGEXP '(?P<INTERFACE>eth\d+) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility > 5) ")
-        assert current_matcher.matches(testEvent)
+        assert current_matcher.matches(test_event)
         
         current_matcher = Matcher("message REGEXP '(?P<INTERFACE>eth[1-9]) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility > 5) ")
-        assert current_matcher.matches(testEvent) == False
+        assert current_matcher.matches(test_event) == False
         
         current_matcher = Matcher("message REGEXP '(?P<INTERFACE>eth[1-9]) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility < 5) ")
-        assert current_matcher.matches(testEvent) == True        
+        assert current_matcher.matches(test_event) == True        
 #  
 #    def test_performance(self):
 #
