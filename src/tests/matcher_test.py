34b423c0358354b6092e0a3ebf6ff1859e4b80d8
Jannis Mosshammer <jannis.mosshammer@netways.de>
2012-12-12 15:11:56 +0100

    pylint fixes

--- src/tests/matcher_test.py
+++ src/tests/matcher_test.py
@@ -1,150 +1,186 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+"""
+EDBC - Message correlation and aggregation engine for passive monitoring events
+Copyright (C) 2012  NETWAYS GmbH
 
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
 import unittest
-import logging
 import time
-
-from event import *
+from event import Event,Matcher
 
 class MatcherTestCase(unittest.TestCase):
+    """ Test cases for the matcher syntax
+
+    """
     
+
     def test_simple_message(self):
-        curMatcher = Matcher("(message is 'i am a testmessage')")
+        """ Tests for simple message checks and regexp
+
+        """
+    
+        current_matcher = Matcher("(message is 'i am a testmessage')")
 
         testEvent = Event("I AM A Testmessage")
-        assert curMatcher.matches(testEvent) == True
+        assert current_matcher.matches(testEvent) == True
 
         testEvent = Event("I AM A non matching Testmessage")
-        assert curMatcher.matches(testEvent) == False
+        assert current_matcher.matches(testEvent) == False
         
-        curMatcher = Matcher("(message is 'i am a testmessage') OR (source in ('snmp')) AND test = 123")        
+        current_matcher = Matcher("(message is 'i am a testmessage') OR (source in ('snmp')) AND test = 123")
         testEvent = Event("I AM A Testmessage")
         # Should be true as the left part is not evaluated
-        assert curMatcher.matches(testEvent) == True
+        assert current_matcher.matches(testEvent) == True
         
         testEvent = Event("I AM A non matching Testmessage")
-        assert curMatcher.matches(testEvent) == False
+        assert current_matcher.matches(testEvent) == False
 
-        curMatcher = Matcher("(message starts with 'i am a testmessage') OR message ends with 'test123'")
+        current_matcher = Matcher("(message starts with 'i am a testmessage') OR message ends with 'test123'")
         
         testEvent = Event("I AM A non matching Testmessage")
-        assert curMatcher.matches(testEvent) == False
+        assert current_matcher.matches(testEvent) == False
 
         testEvent = Event("I AM A Testmessage")
-        assert curMatcher.matches(testEvent) == True
+        assert current_matcher.matches(testEvent) == True
         testEvent = Event("Idsfdsfdsfdsfdsf AM A Testmessage test123")
 
-        assert curMatcher.matches(testEvent) == True
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("(message REGEXP 'Test\w+ [test]+123$' )")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("(message REGEXP 'Test\w+ [test]+123$' )")
+        assert current_matcher.matches(testEvent) == True
 
     def test_matcher_memory(self):
+        """ Tests if regular expression groups are stored in memory after a match
+            
+        """
         testEvent = Event("test123 AM A Testmessage test123")
 
-        curMatcher = Matcher("(message REGEXP '(?P<Group1>.*) AM A Testmessage' )")
-        assert curMatcher.matches(testEvent) == True
-        assert curMatcher["Group1"] == "test123"
+        current_matcher = Matcher("(message REGEXP '(?P<Group1>.*) AM A Testmessage' )")
+        assert current_matcher.matches(testEvent) == True
+        assert current_matcher["Group1"] == "test123"
    
-        
     def test_simple_message_negate(self):
-        curMatcher = Matcher("(message is not 'i am a testmessage')")
+        """ Same like simple message tests, but negated
+
+        """
+        current_matcher = Matcher("(message is not 'i am a testmessage')")
         testEvent = Event("I AM A Testmessage")
-        assert curMatcher.matches(testEvent) == False
+        assert current_matcher.matches(testEvent) == False
 
         testEvent = Event("I AM A non matching Testmessage")
-        assert curMatcher.matches(testEvent) == True
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("(message is not 'i am a testmessage') OR (source in ('snmp')) AND test = 123")        
+        current_matcher = Matcher("(message is not 'i am a testmessage') OR (source in ('snmp')) AND test = 123")        
         testEvent = Event("I AM A Testmessage")
         # Should be true as the left part is not evaluated
-        assert curMatcher.matches(testEvent) == False
+        assert current_matcher.matches(testEvent) == False
         
         testEvent = Event("I AM A non matching Testmessage")
-        assert curMatcher.matches(testEvent) == True
+        assert current_matcher.matches(testEvent) == True
 
-        
-        
     def test_simple_numeric(self):
+        """ Simple tests for numeric values (float and int)
+
+        """
         testEvent = Event("ignore my message", time.ctime(),{
             "severity" : 4,
             "availability" : 0.995
         })
         
-        curMatcher = Matcher("severity > 3")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("severity > 3")
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("severity = 3")
-        assert curMatcher.matches(testEvent) == False
+        current_matcher = Matcher("severity = 3")
+        assert current_matcher.matches(testEvent) == False
         
-        curMatcher = Matcher("severity >= 4")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("severity >= 4")
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("severity <= 4")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("severity <= 4")
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("severity = 4")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("severity = 4")
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("severity != 4")
-        assert curMatcher.matches(testEvent) == False
+        current_matcher = Matcher("severity != 4")
+        assert current_matcher.matches(testEvent) == False
         
-        curMatcher = Matcher("severity != 3")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("severity != 3")
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("availability > 0.99")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("availability > 0.99")
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("availability < 1")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("availability < 1")
+        assert current_matcher.matches(testEvent) == True
         
     def test_simple_set(self):
+        """ Simple tests for set operations 
+
+        """
         testEvent = Event("ignore my message", time.ctime(),{
             "severity" : 6,
             "source" : 'snmp'
         })
-        curMatcher = None
+        current_matcher = None
 
-        curMatcher = Matcher("severity IN (1, 2, 3, 4, 5)")
-        assert curMatcher.matches(testEvent) == False
+        current_matcher = Matcher("severity IN (1, 2, 3, 4, 5)")
+        assert current_matcher.matches(testEvent) == False
 
-        curMatcher = Matcher("severity IN (1, 2, 3, 4, 5, 6)")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("severity IN (1, 2, 3, 4, 5, 6)")
+        assert current_matcher.matches(testEvent) == True
 
-        curMatcher = Matcher("severity NOT IN (1, 2, 3, 4, 5)")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("severity NOT IN (1, 2, 3, 4, 5)")
+        assert current_matcher.matches(testEvent) == True
 
-        curMatcher = Matcher("severity NOT IN (1, 2, 3, 4, 5, 6)")
-        assert curMatcher.matches(testEvent) == False
+        current_matcher = Matcher("severity NOT IN (1, 2, 3, 4, 5, 6)")
+        assert current_matcher.matches(testEvent) == False
 
         
-        curMatcher = Matcher("source IN ('j2ee','snmp')")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("source IN ('j2ee','snmp')")
+        assert current_matcher.matches(testEvent) == True
 
-        curMatcher = Matcher("source NOT IN ('snmp','j2ee')")
-        assert curMatcher.matches(testEvent) == False
+        current_matcher = Matcher("source NOT IN ('snmp','j2ee')")
+        assert current_matcher.matches(testEvent) == False
 
     def test_ip_operators(self):
+        """ IP operator tests
+            
+        """
         testEvent = Event("blobb", time.ctime(),{
             "address": '192.168.67.3',
             
         })
         
-        curMatcher = Matcher("address IN NETWORK '192.168.0.0/16'")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("address IN NETWORK '192.168.0.0/16'")
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("address NOT IN NETWORK '192.168.1.0/24'")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("address NOT IN NETWORK '192.168.1.0/24'")
+        assert current_matcher.matches(testEvent) == True
         
-        curMatcher = Matcher("address IN NETWORK '192.168.1.0/24'")
-        assert curMatcher.matches(testEvent) == False
+        current_matcher = Matcher("address IN NETWORK '192.168.1.0/24'")
+        assert current_matcher.matches(testEvent) == False
         
-        curMatcher = Matcher("address IN IP RANGE '192.168.1.0-192.169.1.1'")
-        assert curMatcher.matches(testEvent) == True
+        current_matcher = Matcher("address IN IP RANGE '192.168.1.0-192.169.1.1'")
+        assert current_matcher.matches(testEvent) == True
 
-        
     def test_mixed(self):
+        """ Tests of several conjunctions and different operators
+
+        """
         testEvent = Event("[LINK DOWN] eth0 on localhost is down.", time.ctime(),{
             "severity" : 7,
             "facility" : 4,
@@ -152,18 +188,18 @@ class MatcherTestCase(unittest.TestCase):
             "program" : "NetworkManager"
         })
         
-        curMatcher = Matcher("message REGEXP '(?P<INTERFACE>eth\d+) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility > 5) ")
-        assert curMatcher.matches(testEvent)
+        current_matcher = Matcher("message REGEXP '(?P<INTERFACE>eth\d+) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility > 5) ")
+        assert current_matcher.matches(testEvent)
         
-        curMatcher = Matcher("message REGEXP '(?P<INTERFACE>eth[1-9]) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility > 5) ")
-        assert curMatcher.matches(testEvent) == False
+        current_matcher = Matcher("message REGEXP '(?P<INTERFACE>eth[1-9]) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility > 5) ")
+        assert current_matcher.matches(testEvent) == False
         
-        curMatcher = Matcher("message REGEXP '(?P<INTERFACE>eth[1-9]) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility < 5) ")
-        assert curMatcher.matches(testEvent) == True        
+        current_matcher = Matcher("message REGEXP '(?P<INTERFACE>eth[1-9]) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility < 5) ")
+        assert current_matcher.matches(testEvent) == True        
 #  
 #    def test_performance(self):
 #
-#        curMatcher = Matcher("message REGEXP '(?P<INTERFACE>eth\d+) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility > 5) AND (address IN NETWORK '192.168.170.0/26' OR address IN IP RANGE '192.168.100.0-192.168.120.255')")
+#        current_matcher = Matcher("message REGEXP '(?P<INTERFACE>eth\d+) on (?P<HOST>\w+ is down)' OR (host IS 'localhost' AND facility > 5) AND (address IN NETWORK '192.168.170.0/26' OR address IN IP RANGE '192.168.100.0-192.168.120.255')")
 #        EVENT_HARDLIMIT_PER_EVENT=0.003
 #        COUNT = 20000
 #        
@@ -180,7 +216,7 @@ class MatcherTestCase(unittest.TestCase):
 #    
 #        now = time.time()
 #        for event in events:
-#            curMatcher.matches(event)
+#            current_matcher.matches(event)
 #        duration = time.time() - now
 #        try:
 #            assert duration/COUNT <= EVENT_HARDLIMIT_PER_EVENT
