93a6cb5e2fd1dac0522c51a8321c3fc00759cc1a
Jannis Moßhammer <jannis.mosshammer@netways.de>
2012-12-20 17:42:10 +0100

    PyLint syntax refactoring and documentation - edbc.py

--- src/edbc.py
+++ src/edbc.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
+"""
+EDBC - Message correlation and aggregation engine for passive monitoring events
+Copyright (C) 2012  NETWAYS GmbH
 
-from config import *;
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
+
+from config import DaemonConfiguration,InstanceFactory;
 
 import logging
 import controller
@@ -9,7 +28,30 @@ import sys
 import os
 
 class Edbc(object):
-    def __daemonize(self):    
+    """ CLI entry point. Parses CLI params via the DaemonConfiguration class. Further object initialization and
+        control is done by the controller.Controller
+
+    """
+
+
+    def __init__(self):
+
+        if not socket.has_ipv6:
+            print >> sys.stderr, 'WARNING: Your python version does not have IPv6 support enabled!'
+
+        self.config = DaemonConfiguration()
+        self.__setup_logging()
+        if self.config["foreground"] == False:
+            self.__daemonize()
+
+        self.instances = InstanceFactory(self.config)
+        controller.Controller(self.config, self.instances)
+
+    def __daemonize(self):
+        """ Makes this process a child process of init
+
+        """
+
         pid = os.fork()
         if not pid > 0:
             cpid = os.fork()
@@ -18,25 +60,16 @@ class Edbc(object):
         else:
             sys.exit(0)
 
-    def __setupLogging(self):
+    def __setup_logging(self):
+        """ When 'log' is not set, logging is done to a logfile, otherwise to stdout
+            @TODO: Loglevel is missing
+        """
+
         if self.config["log"]:
-            logging.basicConfig(level=logging.INFO,filename=self.config["log"])
+            logging.basicConfig(level=logging.INFO, filename=self.config["log"])
         else:
             logging.basicConfig(level=logging.INFO)
-    def __init__(self):
-        if not socket.has_ipv6:
-            print >> sys.stderr, 'WARNING: Your python version does not have IPv6 support enabled!'
-            
-        self.config = DaemonConfiguration()
-        self.__setupLogging()        
-        if self.config["foreground"] == False:
-            self.__daemonize()
-        
-        self.instances = InstanceFactory(self.config)
-        controller.Controller(self.config, self.instances) 
 
-   
-    
             
 if __name__ == "__main__":
-    Edbc();
+    Edbc()
